a2edb87d0f55b95bb8e5e2eef6b0d1a35fb46883
Mustafa KARATAŞ <karatasmustafa918@gmail.com>
2024-12-23 02:17:01 +0300

    update projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import rdt from "../../Assets/Projects/diyabetik-retinopati-2.jpg";
+import dog from "../../Assets/Projects/images.jpeg";
+import wow from "../../Assets/Projects/5Z2eP5Tfe0anTWCtliOV2v.webp";
+import fav from "../../Assets/Projects/images (1).jpeg";
+import mto from "../../Assets/Projects/images (2).jpeg";
 
 function Projects() {
   return (
@@ -15,15 +14,15 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          Yaptığım <strong className="purple">Projeler </strong>
+          Yaptığım Bazı <strong className="purple">Projeler </strong>
         </h1>
         <p style={{ color: "white" }}>
-        Yaptığım veya içinde yer aldığım projeler.
+        Yaptığım veya içinde yer aldığım projelerden bazıları.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={dog}
               isBlog={false}
               title="Dog Scope"
               description="MobileNet teknolojisi kullanılarak sisteme yüklenen köpek fotoğraflarından hangi ırka ait olduğunu gösteren görüntü işleme projesi."
@@ -33,7 +32,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={rdt}
               isBlog={false}
               title="Diabetic Retinopathy "
               description="CNN algoritması ile fundus makinelerinden çekilen retina görüntülerinde diyabet olup olmadığını anlayan makine öğrenmesi projesi."
@@ -43,7 +42,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={wow}
               isBlog={false}
               title="World Of Wonders"
               description="React, NodeJs ve MongoDB kullanarak yapmış olduğum Dünya üzerindeki gezileceke yerleri gösteren Web Sitesi"
@@ -53,17 +52,17 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={fav}
               isBlog={false}
               title="F Arsiv"
               description="React Native, NodeJs ve MongoDB kullanarak yapmış olduğum, insanların filmleri görüp, hakkında bilgi alabildiği bir mobil projesi."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              ghLink="https://github.com/MUSTAFAKARATAS0/F-Arsiv"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={mto}
               isBlog={false}
               title="Maritime-Trade-Routes-Optimization"
               description="Python ve Graph algoritmaları kullanarak seçilen iki liman arasındaki en kısa deniz yolu mesafesini gösteren uygulama. "
